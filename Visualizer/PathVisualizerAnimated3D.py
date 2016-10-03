f3876c0acb772efc48180dd43b95d5bfa362fab9
kevroy314 <kevin.horecka@gmail.com>
2016-10-03 17:34:11 -0500

    Added animations and inversion compatibility
    Animation happens by default.
    
    Space pauses.
    + speed up time
    - slow down time
    Escape to exit (or just close it)
    R to reset
    E to go to end
    
    White lines are forward time, fuchia are backward time
    
    Inversion is now properly accounted for

--- Visualizer/PathVisualizerAnimated3D.py
+++ Visualizer/PathVisualizerAnimated3D.py
@@ -12,11 +12,16 @@ filename = '001_1_1_1_2016-08-29_10-26-03.dat'  # The relative path to the data 
 meta = get_filename_meta_data(filename)  # The meta filename information for convenience
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
+# First we populate a list of each iteration's data
+# This section of code contains some custom binary parser data which won't be explained here
+iterations = read_binary_file(local_directory, filename)
+# Output the iterations count for debugging purposes
+logging.info("Plotting " + str(len(iterations)) + " iterations.")
+
 app = QtGui.QApplication([])
 w = gl.GLViewWidget()
 w.opts['center'] = pg.Qt.QtGui.QVector3D(0, 0, 30)
 w.opts['distance'] = 200
-w.show()
 w.setWindowTitle('Timeline Visualizer')
 
 gx = gl.GLGridItem()
@@ -50,46 +55,58 @@ w.addItem(v1)
 
 
 def make_color_bar(rgb, p, r, s):
-    c = np.array([[rgb]])
-    tex = pg.makeRGBA(c)[0]
+    col = np.array([[rgb]])
+    tex = pg.makeRGBA(col)[0]
     v = gl.GLImageItem(tex)
     v.translate(p[0], p[1], p[2])
     v.scale(s[0], s[1], s[2])
     v.rotate(r[0], r[1], r[2], r[3])
     return v
 
-w.addItem(make_color_bar((255, 255, 0), (19, 0, 19), (90, 1, 0, 0), (5, 15, 0)))
-w.addItem(make_color_bar((255, 0, 0), (19, 15, 19), (90, 1, 0, 0), (5, 15, 0)))
-w.addItem(make_color_bar((0, 255, 0), (19, 30, 19), (90, 1, 0, 0), (5, 15, 0)))
-w.addItem(make_color_bar((0, 0, 255), (19, 45, 19), (90, 1, 0, 0), (5, 15, 0)))
+times = [0, 15, 30, 45]
+if meta['inverse'] == '1':
+    times.reverse()
 
-# First we populate a list of each iteration's data
-# This section of code contains some custom binary parser data which won't be explained here
-iterations = read_binary_file(local_directory, filename)
+w.addItem(make_color_bar((255, 255, 0), (19, times[0], 19), (90, 1, 0, 0), (5, 15, 0)))
+w.addItem(make_color_bar((255, 0, 0), (19, times[1], 19), (90, 1, 0, 0), (5, 15, 0)))
+w.addItem(make_color_bar((0, 255, 0), (19, times[2], 19), (90, 1, 0, 0), (5, 15, 0)))
+w.addItem(make_color_bar((0, 0, 255), (19, times[3], 19), (90, 1, 0, 0), (5, 15, 0)))
 
-white = pg.mkColor("FFFFFF")
-
+forwardColor = (255, 255, 255, 255)
+backwardColor = (255, 0, 255, 255)
+line_color = np.empty((len(iterations), 4))
+line_color_state = np.empty((len(iterations), 4))
 x = []
 y = []
 z = []
-for i in iterations:
+for idx, i in enumerate(iterations):
     x.append(float(i['x']))
     y.append(float(i['z']))
     z.append(float(i['time']))
+    c = forwardColor
+    if i['timescale'] <= 0:
+        c = backwardColor
+    line_color[idx] = pg.glColor(c)
+    line_color_state[idx] = pg.glColor((0, 0, 0, 0))
 
 pts = np.vstack([x, y, z]).transpose()
-plt = gl.GLLinePlotItem(pos=pts, antialias=True)
+plt = gl.GLLinePlotItem(pos=pts, color=line_color, mode='line_strip', antialias=True)
 w.addItem(plt)
 
-# Fall = 1, Fly = 0, Stay = 2
-items = [{'direction': 1, 'pos': (18, -13, 4), 'color': (255, 255, 0)},
-         {'direction': 0, 'pos': (-13, 9, 10), 'color': (255, 255, 0)},
-         {'direction': 0, 'pos': (-10, -2, 16), 'color': (255, 0, 0)},
-         {'direction': 1, 'pos': (6, -2, 25), 'color': (255, 0, 0)},
-         {'direction': 1, 'pos': (17, -8, 34), 'color': (0, 255, 0)},
-         {'direction': 0, 'pos': (-2, -7, 40), 'color': (0, 255, 0)},
-         {'direction': 1, 'pos': (-15, -15, 46), 'color': (0, 0, 255)},
-         {'direction': 0, 'pos': (6, 18, 51), 'color': (0, 0, 255)},
+times = [4, 10, 16, 25, 34, 40, 46, 51]
+directions = [1, 0, 0, 1, 1, 0, 1, 0]  # Fall = 1, Fly = 0, Stay = 2
+if meta['inverse'] == '1':
+    times.reverse()
+    directions.reverse()
+
+items = [{'direction': directions[0], 'pos': (18, -13, times[0]), 'color': (255, 255, 0)},
+         {'direction': directions[1], 'pos': (-13, 9, times[1]), 'color': (255, 255, 0)},
+         {'direction': directions[2], 'pos': (-10, -2, times[2]), 'color': (255, 0, 0)},
+         {'direction': directions[3], 'pos': (6, -2, times[3]), 'color': (255, 0, 0)},
+         {'direction': directions[4], 'pos': (17, -8, times[4]), 'color': (0, 255, 0)},
+         {'direction': directions[5], 'pos': (-2, -7, times[5]), 'color': (0, 255, 0)},
+         {'direction': directions[6], 'pos': (-15, -15, times[6]), 'color': (0, 0, 255)},
+         {'direction': directions[7], 'pos': (6, 18, times[7]), 'color': (0, 0, 255)},
          {'direction': 2, 'pos': (14, 6, 0), 'color': (128, 0, 128)},
          {'direction': 2, 'pos': (-2, 10, 0), 'color': (128, 0, 128)}, ]
 pos = np.empty((len(items), 3))
@@ -114,6 +131,99 @@ for idx, i in enumerate(items):
 
 sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
 w.addItem(sp1)
+
+w.show()
+logging.info("Showing plot. Close plot to exit program.")
+
+idx = 0
+num_points_to_update = 5
+saved_points_to_update = 0
+paused = False
+
+
+def speed_up():
+    global num_points_to_update, paused
+    if not paused:
+        num_points_to_update += 5
+        logging.info("Setting speed to " + str(num_points_to_update) + " points per tick.")
+
+
+def speed_down():
+    global num_points_to_update, paused
+    if not paused:
+        num_points_to_update -= 5
+        logging.info("Setting speed to " + str(num_points_to_update) + " points per tick.")
+
+
+def pause():
+    global num_points_to_update, saved_points_to_update, paused
+    if not paused:
+        logging.info("Paused.")
+        saved_points_to_update = num_points_to_update
+        num_points_to_update = 0
+        paused = True
+    else:
+        logging.info("Unpaused.")
+        num_points_to_update = saved_points_to_update
+        saved_points_to_update = -0.5
+        paused = False
+
+
+def reset():
+    global idx, line_color_state
+    logging.info("Resetting to time zero.")
+    idx = 0
+    for index in range(0, len(line_color_state)-1):
+        line_color_state[index] = (0, 0, 0, 0)
+
+
+def go_to_end():
+    global idx, line_color_state, line_color
+    logging.info("Going to end.")
+    idx = len(line_color_state) - 1
+    for index in range(0, len(line_color_state) - 1):
+        line_color_state[index] = line_color[index]
+
+
+def close_all():
+    global timer, app
+    logging.info("User Shutdown Via Button Press")
+    timer.stop()
+    app.closeAllWindows()
+
+sh = QtGui.QShortcut(QtGui.QKeySequence("+"), w, speed_up)
+sh.setContext(QtCore.Qt.ApplicationShortcut)
+sh = QtGui.QShortcut(QtGui.QKeySequence("-"), w, speed_down)
+sh.setContext(QtCore.Qt.ApplicationShortcut)
+sh = QtGui.QShortcut(QtGui.QKeySequence(" "), w, pause)
+sh.setContext(QtCore.Qt.ApplicationShortcut)
+sh = QtGui.QShortcut(QtGui.QKeySequence("R"), w, reset)
+sh.setContext(QtCore.Qt.ApplicationShortcut)
+sh = QtGui.QShortcut(QtGui.QKeySequence("E"), w, go_to_end)
+sh.setContext(QtCore.Qt.ApplicationShortcut)
+sh = QtGui.QShortcut(QtGui.QKeySequence("Escape"), w, close_all)
+sh.setContext(QtCore.Qt.ApplicationShortcut)
+
+
+def update():
+    global plt, idx, timer
+    for _ in range(0, abs(num_points_to_update)):
+        if num_points_to_update > 0:
+            line_color_state[idx] = line_color[idx]
+            idx += 1
+        else:
+            line_color_state[idx] = (0, 0, 0, 0)
+            idx -= 1
+        if idx < 0:
+            idx = 0
+        elif idx >= len(line_color):
+            idx = len(line_color) - 1
+            break
+    plt.setData(color=line_color_state)
+timer = QtCore.QTimer()
+# noinspection PyUnresolvedReferences
+timer.timeout.connect(update)
+timer.start(1)
 
 
 if __name__ == '__main__':
